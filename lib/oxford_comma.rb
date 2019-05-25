ab65358bef379b940f00551289ebc0d457ed9801
kevin lang <langkevina@gmail.com>
2019-05-25 11:48:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
     return array[0]
   elsif (array.size == 2)
-    return array.to_s.split("and")
+    return array.to_s
   elsif array.size > 1
     cnt = 0
     myArr = Array.new
