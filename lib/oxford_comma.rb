42c98e35873f16e23e3e676628c90b3f7ed111d1
kevin lang <langkevina@gmail.com>
2019-05-25 11:44:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,13 @@
 def oxford_comma(array)
   if array.size == 1
     return array[0]
-  elsif array.size == 1
+  elsif (array.size == 1) ||  (array.size == 2) 
     return array.split("and")
   elsif array.size > 1
     cnt = 0
     myArr = Array.new
     myStr = ""
     array.each do |item|
-
       if cnt < array.size - 1
         myStr = "" + myStr + item + ", "
       elsif
