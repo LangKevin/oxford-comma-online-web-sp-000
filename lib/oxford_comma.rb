da407fa6b68f355518e0de80de9f26cda3ec4dcb
kevin lang <langkevina@gmail.com>
2019-05-25 11:49:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,7 @@ def oxford_comma(array)
     myStr = ""
     array.each do |item|
       if array.size == 2
+        myStr = "" + myStr + item 
         cnt += 1
       end
       if cnt < array.size - 1
